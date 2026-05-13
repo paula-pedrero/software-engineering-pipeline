9982124e12bb857d673fb0848af18fa5bd5cb92e
RegNugg <didaccis@gmail.com>
2026-05-13 16:20:01 +0200

    flake8 errors

--- calculator.py
+++ calculator.py
@@ -1,29 +1,37 @@
 def add(a, b):
     return a + b
 
+
 def sub(a, b):
     return a - b
+
 
 def power(a,b):
     return a ** b
 
+
 def modulo(a, b):
     return a % b
+
 
 def div(a, b):
     return a / b
 
+
 def average(a, b):
     return (a+b)/2
 
+
 def multiply(a, b):
     return a * b
+
 
 def factorial(n):
     result = 1
     for i in range(1, n + 1):
         result *= i
     return result
+
 
 def root(a, b):
     if b % 2 == 0 and  a < 0:

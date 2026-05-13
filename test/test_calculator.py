a368bbe9c9bc5884b2d3fd6c4918a15b7c755cf0
Isaac Amorós <isaac.amoros01@estudiant.upf.edu>
2026-05-13 15:45:59 +0200

    Add multiply function and tests

--- test/test_calculator.py
+++ test/test_calculator.py
@@ -1,6 +1,9 @@
-from calculator import add, sub
+from calculator import add, sub, multiply
 
 def test_add():
     assert add(2, 3) == 5
 def test_sub():
     assert sub(10, 4) == 6
+def test_multiply():
+    assert multiply(3, 4) == 12
+    assert multiply(0, 5) == 0

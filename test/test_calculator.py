34b1f70be63c4d8636dbbed7921f473b6e4cef41
paula-pedrero <paula.sanchez16@estudiant.upf.edu>
2026-05-13 15:36:49 +0200

    Add average operation

--- test/test_calculator.py
+++ test/test_calculator.py
@@ -1,6 +1,9 @@
-from calculator import add, sub
+from calculator import add, sub, average
 
 def test_add():
     assert add(2, 3) == 5
 def test_sub():
     assert sub(10, 4) == 6
+
+def test_average():
+    assert average(0,10) == 5

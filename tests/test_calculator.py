8684fdfb1d3536856a52c5335fd4283a6f03b5a4
RegNugg <didaccis@gmail.com>
2026-05-13 16:08:43 +0200

    Clean up

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,4 +1,4 @@
-from calculator import add, sub, modulo, multiply, average, root, power, factorial, divide
+from calculator import add, sub, modulo, multiply, average, root, power, factorial, div
 
 
 def test_modulo():
@@ -11,22 +11,26 @@ def test_divide():
 
 def test_add():
     assert add(2, 3) == 5
+
 def test_sub():
     assert sub(10, 4) == 6
 
 def test_average():
     assert average(0,10) == 5
+
 def test_multiply():
     assert multiply(3, 4) == 12
     assert multiply(0, 5) == 0
-    
+
 def test_power():
     assert power(2,2) == 4
     assert power(2,3) == 16
 
+
 def test_root():
     assert root(100, 2) == 10
     assert root(-5, 2) == "If a is negative b can't be even!"
+    
 def test_factorial():
     assert factorial(4) == 24
-    assert fatorial(0) == 1
+    assert factorial(0) == 1

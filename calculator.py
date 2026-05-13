8b8daa3aafff07ff83a05b4944cbb9a8111af204
RegNugg <didaccis@gmail.com>
2026-05-13 15:37:34 +0200

    Root added

--- calculator.py
+++ calculator.py
@@ -4,3 +4,8 @@ def add(a, b):
 
 def sub(a, b):
     return a - b
+
+def root(a, b):
+    if b % 2 == 0 and  a < 0:
+        raise ValueError ("If a is negative b can't be pair!")
+    return a**(1/b)

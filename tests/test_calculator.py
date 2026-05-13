793d08ebb7ce93b6227114106fdcd8b4bf0dbb12
carlaferrer03 <167971753+carlaferrer03@users.noreply.github.com>
2026-05-13 16:02:27 +0200

    Update test_calculator.py

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,4 +1,7 @@
 from calculator import add, sub, modulo
+from calculator import add, sub, average
+from calculator import add, sub, multiply
+from calculator import add, sub, root
 
 def test_modulo():
     assert modulo(10, 3) == 1
@@ -7,3 +10,20 @@ def test_modulo():
 def test_divide():
     assert div(10, 2) == 5
     assert div(9, 3) == 3
+
+def test_add():
+    assert add(2, 3) == 5
+def test_sub():
+    assert sub(10, 4) == 6
+
+def test_average():
+    assert average(0,10) == 5
+def test_multiply():
+    assert multiply(3, 4) == 12
+    assert multiply(0, 5) == 0
+def test_root():
+    assert root(100, 2) == 10
+    assert root(-5, 2) == "If a is negative b can't be even!"
+def test_factorial():
+    assert factorial(4) == 24
+    assert fatorial(0) == 1
